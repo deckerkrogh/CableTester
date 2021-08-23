e0180c8688a827314b3a1c56c8dcb413d991778e
Decker Krogh <DeckerK@autometrix.local>
2021-08-23 10:43:16 -0700

    removed bomstrip

--- bomstrip.ps1
+++
@@ -1,7 +0,0 @@
-$files=get-childitem -Path . -Include @("*.h","*.cpp") -Recurse
-foreach ($f in $files)
-{
-	(Get-Content $f.PSPath) | 
-	Foreach-Object {$_ -replace "\xEF\xBB\xBF", ""} | 
-	Set-Content $f.PSPath
-}
